7e68669edab458fe0433e6582daad86bb7915b3e
SaGeDeveloper <55732192+SaGeDeveloper@users.noreply.github.com>
2020-02-13 20:36:29 +0300

    Update
    Обновил репозиторий до уровня своего. Это включает в себя доделанный 2 уровень, 3 уровень, экраны загрузки, прототип главного меню, исправление бага с полетом и т.д.

---
+++ MagnetGun/Assets/Scripts/loadingCameraRotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadingCameraRotation : MonoBehaviour {
+		public GameObject cam;
+		public float sensitivity;
+		public float clampAngle;
+		float rotX = 0f;
+		float rotY = 0f;
+		List<Collider> col = new List<Collider>();
+
+		void FixedUpdate(){
+			rotY += Input.GetAxis("Mouse X") * Time.deltaTime * sensitivity;
+			rotX -= Input.GetAxis("Mouse Y") * Time.deltaTime * sensitivity;
+			rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+			cam.transform.localRotation = Quaternion.Euler(rotX, 0f , 0.0f);
+			transform.rotation = Quaternion.Euler(0f, rotY , 0.0f);
+		}
+		// Use this for initialization
+		void Start () {
+
+		}
+	}
