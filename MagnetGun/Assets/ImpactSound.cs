cdc7bba536f7f3bb874453c2e9232555873d79f8
SaGeDeveloper <55732192+SaGeDeveloper@users.noreply.github.com>
2020-03-28 22:21:12 +0300

    Sound1 (Impact)
    Звуковое сопровождение для куба и дверей, экран загрузки для ур. 6

---
+++ MagnetGun/Assets/ImpactSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpactSound : MonoBehaviour {
+	public AudioSource sound;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter (Collision col) {
+		if (col.relativeVelocity.magnitude > 1 && col.collider.gameObject.tag != "Player") {
+			sound.Play ();
+		}
+	}
+}
--- MagnetGun/Assets/Scripts/Logic/Door_logic.cs
+++ MagnetGun/Assets/Scripts/Logic/Door_logic.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Door_logic : MonoBehaviour {
 	public Element_Logic logic;
 	public Animator anim;
+	public AudioSource soundOpen;
+	public bool mute;
+	private string ps = "Closed";
 	// Use this for initialization
 	void Start () {
 		
@@ -13,5 +16,29 @@ public class Door_logic : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		anim.SetBool("isOpen", logic.value);
+		if (!mute) {
+			if (anim.GetCurrentAnimatorStateInfo (0).IsName ("Opening")) {
+				if (ps != "Opening") {
+					ps = "Opening";
+					soundOpen.Play ();
+				}
+			}
+			if (anim.GetCurrentAnimatorStateInfo (0).IsName ("Closing")) {
+				if (ps != "Closing") {
+					ps = "Closing";
+					soundOpen.Play ();
+				}
+			}
+			if (anim.GetCurrentAnimatorStateInfo (0).IsName ("Open")) {
+				if (ps != "Open") {
+					ps = "Open";
+				}
+			}
+			if (anim.GetCurrentAnimatorStateInfo (0).IsName ("Closed")) {
+				if (ps != "Closed") {
+					ps = "Closed";
+				}
+			}
+		}
 	}
 }
